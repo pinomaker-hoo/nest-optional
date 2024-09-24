83ea8c3fa46d75b4f605c96f02b5a4186b426c0b
pinomaker-hoo <inhoo23@naver.com>
2024-09-24 17:57:57 +0900

    feat : version 1.0.0

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+class Optional<T> {
+  private value: T | null;
+
+  private constructor(value: T | null) {
+    this.value = value;
+  }
+
+  static of<T>(value: T | null): Optional<T> {
+    return new Optional(value);
+  }
+
+  isPresent(): boolean {
+    return this.value !== null;
+  }
+
+  get(): T {
+    if (this.value === null) {
+      throw new Error("Value is not present");
+    }
+    return this.value;
+  }
+
+  orElseThrow<E extends Error>(
+    exception: new (...args: any[]) => E,
+    message: string
+  ): T {
+    if (this.value === null) {
+      throw new exception(message);
+    }
+    return this.value;
+  }
+}
+
+export default Optional;
